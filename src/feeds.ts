0a4efe64828ab8a78192f84fc46233842188f900
KO <karloskar+github@karloskar.org>
2024-05-14 15:43:30 +0930

    Update feeds.ts

--- src/feeds.ts
+++ src/feeds.ts
@@ -3,4 +3,7 @@ export const feeds = [
   'https://bitterswede.com/feed/',
   'https://www.stugr.com/feed.xml',
   'https://stilgherrian.com/feed/',
+  'https://www.australiangeographic.com.au/feed/',
+  'https://www.productsafety.gov.au/rss/recalls.xml',
+  
 ]
