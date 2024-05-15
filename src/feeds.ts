f105d4152cd208057d03bcc99c012f32b5607069
KO <karloskar+github@karloskar.org>
2024-05-15 09:37:26 +0930

    Update feeds.ts
    Made it pretty

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,12 +1,29 @@
 export const feeds = [
+  // -----------------------------------------
+  // Friends, Family, Commmunity
+  // -----------------------------------------
   'https://www.kra.org.au/feed/',
-  'https://bitterswede.com/feed/',
   'https://www.stugr.com/feed.xml',
   'https://stilgherrian.com/feed/',
+
+  
+  // -----------------------------------------
+  // Australia
+  // -----------------------------------------
   'https://www.australiangeographic.com.au/feed/',
   'https://www.productsafety.gov.au/rss/recalls.xml',
+
+
+  // -----------------------------------------
+  // Spaceeeee
+  // -----------------------------------------
   'https://blogs.nasa.gov/sunspot/feed/',
   'https://blogs.nasa.gov/artemis/feed/',
   'https://blogs.nasa.gov/webb/feed/',
   'https://hackaday.com/category/space/feed/',
+  
+  // -----------------------------------------
+  // Misc
+  // -----------------------------------------
+  'https://postsecret.com/feed/',
 ]
