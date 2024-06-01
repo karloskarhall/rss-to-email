4d6a0be1a7b03331e8dd1e68a352679097e234c8
KO <karloskar+github@karloskar.org>
2024-06-01 11:30:50 +0930

    Update feeds.ts
    Added YouTube links

--- src/feeds.ts
+++ src/feeds.ts
@@ -6,7 +6,6 @@ export const feeds = [
   'https://www.stugr.com/feed.xml',
   'https://stilgherrian.com/feed/',
   'https://fernandogros.com/feed/',
-
   
   // -----------------------------------------
   // Australia
@@ -28,6 +27,18 @@ export const feeds = [
   // Funn
   // -----------------------------------------
   'https://xkcd.com/atom.xml',
+
+  // -----------------------------------------
+  // YouTube
+  // -----------------------------------------
+
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCSFaYYQzNMLo2U6rSNLpghg', //Torbjörn Åhman
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UC4EQCwfH54ieNPswtXh5K0w', //My Mechanics Insights
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCMrMVIBtqFW6O0-MWq26gqw', //My Mechanics
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCPzFLpOblZEaIx2lpym1l1A', //French Guy Cooking
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UC5NO8MgTQKHAWXp6z8Xl7yQ', //This Old Tony
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCm325cMiw9B15xl22_gr6Dw', //Beau Miles
+  
   
   // -----------------------------------------
   // Misc
