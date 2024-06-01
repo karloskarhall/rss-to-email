8912b9b9a1a5ffbf8d92298d2a2ba1617842b0f1
KO <karloskar+github@karloskar.org>
2024-06-02 08:48:06 +0930

    Update feeds.ts
    More YouTube

--- src/feeds.ts
+++ src/feeds.ts
@@ -32,13 +32,30 @@ export const feeds = [
   // YouTube
   // -----------------------------------------
 
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCSFaYYQzNMLo2U6rSNLpghg', //Torbjörn Åhman
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UC4EQCwfH54ieNPswtXh5K0w', //My Mechanics Insights
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCMrMVIBtqFW6O0-MWq26gqw', //My Mechanics
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UCPzFLpOblZEaIx2lpym1l1A', //French Guy Cooking
-  'https://www.youtube.com/feeds/videos.xml?channel_id=UC5NO8MgTQKHAWXp6z8Xl7yQ', //This Old Tony
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCe1Aj6VEO299Yq4WkXdoD3Q', //Alexandre Chappel
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCc9CjaAjsMMvaSghZB7-Kog', //Beard Meats Food
   'https://www.youtube.com/feeds/videos.xml?channel_id=UCm325cMiw9B15xl22_gr6Dw', //Beau Miles
-  
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCa0BBg2T82phyhXwa5qlBZQ', //Beauisms (Beau Miles 2nd)
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UC6I0KzAD7uFTL1qzxyunkvA', //Blacktail Studio
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCC76FBg2d-5NWFD8aK4ql5A', //Carl Rogers
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UC0e3QhIYukixgh5VVpKHH9Q', //Code Bullet
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCPzFLpOblZEaIx2lpym1l1A', //French Guy Cooking
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCeJFgNahi--FKs0oJyeRDEw', //Hybrid Calisthenics
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCRix1GJvSBNDpEFY561eSzw', //Laura Kampf
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UC_gSotrFVZ_PiAxo3fTQVuQ', //Magnus Midtbø
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UC6dr32gbapkzgw_Eyr6VsJw', //Mark Lewis Fitness
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCIRiWCPZoUyZDbydIqitHtQ', //Mike Boyd
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCMrMVIBtqFW6O0-MWq26gqw', //My Mechanics
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UC4EQCwfH54ieNPswtXh5K0w', //My Mechanics Insights (My Mechanics 2nd)
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UC4eYXhJI4-7wSWc8UNRwD4A', //NPR Tiny Desk Concerts
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UC5rT7F0PGNuD54rJ9kzgWzw', //Kris Harbour Natural Building
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UC1yNl2E66ZzKApQdRuTQ4tw', //Sabine Hossenfelder
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCRtsZ5Iak9wSLsQLQ3XOAeA', //SciManDan
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UC5NO8MgTQKHAWXp6z8Xl7yQ', //This Old Tony
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCSFaYYQzNMLo2U6rSNLpghg', //Torbjörn Åhman
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCD2VuGlPSfBDFGKhxviv1Zg', //VEK (Russian Music)
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCjGiP1mw4T6Ra4VgIsEjsTw', //Woke Spokes
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UCZgsMhQWvCdudLK8Yn1aTfQ', //Yogev Gabay 
   
   // -----------------------------------------
   // Misc
